bebbd9c1870ae9444e3fac2c1bbea95b83057589
Sian Finlay <finlaysian@gmail.com>
2016-04-07 02:57:45 +0100

    solved js problem
    remember they must be added as packages or meteor can’t use them

--- CvsH.js
+++ CvsH.js
@@ -2,18 +2,18 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.home.helpers({
+ /* Template.home.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.home.events({
+ Template.home.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
-  });
+  });*/
 }
 
 if (Meteor.isServer) {
---
+++ client/lib/modal.js
@@ -0,0 +1,21 @@
+//modal js
+Meteor.startup( function(){
+
+	  $("#modal-1").on("change", function() {
+	    if ($(this).is(":checked")) {
+	      $("body").addClass("modal-open");
+	    } else {
+	      $("body").removeClass("modal-open");
+	    }
+	  });
+
+	  $(".modal-fade-screen, .modal-close").on("click", function() {
+	    $(".modal-state:checked").prop("checked", false).change();
+	  });
+
+	  $(".modal-inner").on("click", function(e) {
+	    e.stopPropagation();
+	  });
+
+});
+
