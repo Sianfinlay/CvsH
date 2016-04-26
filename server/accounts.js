ae7818e2df4b5afca522f15e876a40877ee12252
Sian Finlay <finlaysian@gmail.com>
2016-04-26 18:49:17 +0100

    used old code instead of new for twitter login

--- server/accounts.js
+++ server/accounts.js
@@ -1,6 +1,8 @@
 // facebook login
 ServiceConfiguration.configurations.remove({service: "facebook"});
+ServiceConfiguration.configurations.remove({service: "twitter"});
 
+// facebook
 ServiceConfiguration.configurations.insert({
   service: "facebook",
   appId: "1708230562789289",
@@ -8,11 +10,7 @@ ServiceConfiguration.configurations.insert({
 });
 
 
-// twitter login
-Accounts.loginServiceConfiguration.remove({
-  service: "twitter"
-});
-Accounts.loginServiceConfiguration.insert({
+ServiceConfiguration.configurations.insert({
   service: "twitter",
   consumerKey: "YHsL2VwXpWxmivnItO7nVoUqR",
   secret: "HH0jVAugFoaQPnjf0pnSDAg7zMtYNOJmg781AYXAIEWHggvbGO"
